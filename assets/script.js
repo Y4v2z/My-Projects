d1cd1c616c5ed0322e3f943dd8a3173ce73686a6
Yavuz Avci <yavuzavci01@gmail.com>
2023-01-10 15:59:18 +0200

    page created

---
+++ assets/script.js
@@ -0,0 +1,33 @@
+// var myCarousel = document.getElementById("slider1");
+// var carousel = new bootstrap.Carousel(myCarousel, {
+//     interval: 5000,
+//     wrap: true,
+//     keyboard: false,
+//     touch: true,
+//     pause: "hover"
+// });
+// document.getElementById("prev").addEventListener("click", function () {
+//     carousel.prev();
+// })
+// document.getElementById("next").addEventListener("click", function () {
+//     carousel.next();
+// })
+// // pause,cycle gibi metotlarla da aynısı yapılabilir
+// myCarousel.addEventListener("slide.bs.carousel", function (e) {
+//     console.log("slide öncesi");
+//     console.log(e.direction); //slide nereye gidiyor
+//     console.log(e.from);//nreden gelmiş
+//     console.log(e.to);//sıradaki slayt ne bunlar consola yazdırılabilir
+// })
+
+// myCarousel.addEventListener("slid.bs.carousel", function (e) {
+//     console.log("slide sonrası");
+// })
+
+
+// //interval:  slider geçisleri ayarlanır
+// //    wrap: false slider ın otomatik olarak başa dönmesi engellenir
+// //    keyboard: slider ın klavyeye karşı tepk, vermesini önlersin
+// //    touch:telefonlarda elle geçiş yapılsın
+// //    pause: üzerine geldiğinde durmasını sağlar
+// //    
